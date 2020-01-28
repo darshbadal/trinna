e9e84442ae87b7f320c27060d6673ab312fec249
DarshBadal <darshan2002@hotmail.nl>
2020-01-28 11:18:38 +0100

    database aagemaakt en eerste stap webpage

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController extends AbstractController
+{
+    public function homepage()
+    {
+        return new Response('sdad');
+    }
+}
